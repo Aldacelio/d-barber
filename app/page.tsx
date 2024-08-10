21fe3333b323563cb9c8154505a8ded113da4255
Aldacelio <aldacelio@alu.ufc.br>
2024-08-10 03:22:29 -0300

    feat: add BookingItem component

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,10 @@ import { Button } from "./_components/ui/button"
 import { Input } from "./_components/ui/input"
 import Image from "next/image"
 import { Card, CardContent } from "./_components/ui/card"
-import { Badge } from "./_components/ui/badge"
-import { Avatar, AvatarImage } from "./_components/ui/avatar"
 import { db } from "./_lib/prisma"
 import BarbershopItem from "./_components/barbershop-item"
 import QuickSearch from "./_components/quick-search"
+import BookingItem from "./_components/booking-item"
 
 const Home = async () => {
   //chamar o banco de dados
@@ -51,32 +50,7 @@ const Home = async () => {
         </div>
 
         {/* Agendamento */}
-        <h2 className="upercase text-xxs mb-3 mt-6 font-bold text-gray-400">
-          Agendamentos
-        </h2>
-        <Card>
-          <CardContent className="flex justify-between p-0">
-            {/* Esquerda */}
-            <div className="flex flex-col gap-2 py-5 pl-5">
-              <Badge className="w-fit">Confirmado</Badge>
-              <h3 className="font-semibold">Corte de Cabelo</h3>
-
-              <div className="flex items-center gap-2">
-                <Avatar>
-                  <AvatarImage src="https://github.com/diego3g.png" />
-                </Avatar>
-                <p className="text-sm">Barbearia D</p>
-              </div>
-            </div>
-
-            {/* Direita */}
-            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
-              <p className="text-sm">Agosto</p>
-              <p className="text-2xl">05</p>
-              <p className="text-sm">20:00</p>
-            </div>
-          </CardContent>
-        </Card>
+        <BookingItem />
 
         {/* Recomendados */}
         <h2 className="upercase text-xxs mb-3 mt-6 font-bold text-gray-400">
