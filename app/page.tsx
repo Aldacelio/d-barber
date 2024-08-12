60ba8d5429371b58d5120ee3147de4118660a3a1
Aldacelio <aldacelio@alu.ufc.br>
2024-08-12 13:25:17 -0300

    feat: ensure user can search for barbershops

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import { db } from "./_lib/prisma"
 import BarbershopItem from "./_components/barbershop-item"
 import { quickSearchOptions } from "./_constants/search"
 import BookingItem from "./_components/booking-item"
+import Search from "./_components/search"
 
 const Home = async () => {
   //chamar o banco de dados
@@ -26,11 +27,8 @@ const Home = async () => {
         <p>Quarta-feira, 07 de agosto.</p>
 
         {/* Busca */}
-        <div className="mt-6 flex items-center gap-2">
-          <Input placeholder="search" />
-          <Button>
-            <SearchIcon />
-          </Button>
+        <div className="mt-6 gap-2">
+          <Search />
         </div>
 
         {/* Busca rapida */}
